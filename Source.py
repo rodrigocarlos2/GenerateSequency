0f11ce573cc7df4eb9c8e347bf5928b52897173c
Rodrigo Carlos Carvalho Lima Barbosa Leal <rodrigo19962010@live.com>
2017-07-13 15:01:40 -0300

    Add source

---
+++ Source.py
@@ -0,0 +1,16 @@
+
+a1 = input('Write the first element: ')
+
+a1 = int(a1)
+
+an = input('Write the last element: ')
+
+an = int(an)
+
+r = input ('Write the reason: ')
+
+r = int(r)
+
+while a1<=an:
+	print(a1)
+	a1 = a1+r
